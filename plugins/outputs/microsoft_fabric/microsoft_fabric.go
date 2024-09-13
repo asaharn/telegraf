d26ab7aec68f90be9ad4ed1a78e5067a1f058053
asaharn <asaharn@microsoft.com>
2024-09-13 23:40:45 +0530

    MS Fabric implementation

--- plugins/outputs/microsoft_fabric/microsoft_fabric.go
+++ plugins/outputs/microsoft_fabric/microsoft_fabric.go
@@ -1,22 +1,28 @@
 //go:generate ../../../tools/readme_config_includer/generator
-package azure_data_explorer
+package microsoft_fabric
 
 import (
 	_ "embed"
 	"errors"
+	"fmt"
 	"strings"
+	"time"
 
 	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/config"
 	"github.com/influxdata/telegraf/plugins/outputs"
 	ADX "github.com/influxdata/telegraf/plugins/outputs/azure_data_explorer"
-	ES "github.com/influxdata/telegraf/plugins/outputs/azure_event_stream"
+	EH "github.com/influxdata/telegraf/plugins/outputs/event_hubs"
 )
+
+//go:embed sample.conf
+var sampleConfig string
 
 type MicrosoftFabric struct {
 	ConnectionString  string                 `toml:"connection_string"`
 	Log               telegraf.Logger        `toml:"-"`
 	ADXConf           *ADX.AzureDataExplorer `toml:"adx_conf"`
-	ESConf            *ES.EventStream        `toml:"es_conf"` //TODO: implement Eventstream plugin
+	EHConf            *EH.EventHubs          `toml:"eh_conf"`
 	FabricSinkService telegraf.Output
 }
 
@@ -32,7 +38,7 @@ func (m *MicrosoftFabric) Connect() error {
 
 // SampleConfig implements telegraf.Output.
 func (m *MicrosoftFabric) SampleConfig() string {
-	return m.FabricSinkService.SampleConfig()
+	return sampleConfig
 }
 
 // Write implements telegraf.Output.
@@ -48,23 +54,33 @@ func (m *MicrosoftFabric) Init() error {
 	}
 
 	if strings.HasPrefix(ConnectionString, "Endpoint=sb") {
-		m.Log.Info("Detected EventStream endpoint, using EventStream output plugin")
-		m.ESConf.Init()
-		m.FabricSinkService = m.ESConf
+		m.Log.Info("Detected EventHub endpoint, using EventHub output plugin")
+		m.EHConf.ConnectionString = ConnectionString
+		m.EHConf.Log = m.Log
+		m.EHConf.Init()
+		m.FabricSinkService = m.EHConf
 	} else {
 		m.Log.Info("Detected Kusto endpoint, using Kusto output plugin")
+		//Setting up the AzureDataExplorer plugin initial properties
 		m.ADXConf.Endpoint = ConnectionString
+		m.ADXConf.Log = m.Log
 		m.ADXConf.Init()
 		m.FabricSinkService = m.ADXConf
 	}
-
 	return nil
-
 }
 
 func init() {
 
 	outputs.Add("microsoft_fabric", func() telegraf.Output {
-		return &MicrosoftFabric{}
+		return &MicrosoftFabric{
+			ADXConf: &ADX.AzureDataExplorer{
+				Timeout:      config.Duration(20 * time.Second),
+				CreateTables: true,
+			},
+			EHConf: &EH.EventHubs{
+				Timeout: config.Duration(30 * time.Second),
+			},
+		}
 	})
 }
